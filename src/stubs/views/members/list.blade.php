d193f405a2d58a4581c533eed01d0310c0081244
oliuz <geekrosado@gmail.com>
2020-06-01 12:29:27 -0400

    Update Members View

--- src/stubs/views/members/list.blade.php
+++ src/stubs/views/members/list.blade.php
@@ -2,102 +2,115 @@
 
 @section('content')
     <div class="container">
+        @include('teamwork.includes.messages')
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-body">
-                        <h1 class="card-title">
-                            Members of team "{{$team->name}}"
-                            <a href="{{route('teams.index')}}" class="btn btn-sm btn-secondary float-right">
-                                <i class="fa fa-arrow-left"></i> Back
-                            </a>
-                        </h1>
-                        <table class="table table-striped">
-                            <thead class="thead-dark">
-                            <tr>
-                                <th>Name</th>
-                                <th>Action</th>
-                            </tr>
-                            </thead>
-                            @foreach($team->users AS $user)
-                                <tr>
-                                    <td>{{$user->name}}</td>
-                                    <td>
-                                        @if(auth()->user()->isOwnerOfTeam($team))
-                                            @if(auth()->user()->getKey() !== $user->getKey())
-                                                <form style="display: inline-block;"
-                                                      action="{{route('teams.members.destroy', [$team, $user])}}"
-                                                      method="post">
-                                                    {!! csrf_field() !!}
-                                                    <input type="hidden" name="_method" value="DELETE"/>
-                                                    <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i>
-                                                        Delete
-                                                    </button>
-                                                </form>
-                                            @endif
-                                        @endif
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-                <br>
-                <div class="card">
-                    <div class="card-body">
-                        <h1 class="card-title">Pending invitations</h1>
-                        <table class="table table-striped">
-                            <thead class="thead-dark">
-                            <tr>
-                                <th>E-Mail</th>
-                                <th>Action</th>
-                            </tr>
-                            </thead>
-                            @foreach($team->invites AS $invite)
-                                <tr>
-                                    <td>{{$invite->email}}</td>
-                                    <td>
-                                        <a href="{{route('teams.members.resend_invite', $invite)}}"
-                                           class="btn btn-sm btn-default">
-                                            <i class="fa fa-envelope-o"></i> Resend invite
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </table>
-                    </div>
-                </div>
-
-                <br>
-                <div class="card">
-                    <div class="card-body">
-                        <h1 class="card-title">Invite to team "{{$team->name}}"</h1>
-                        <form class="form-horizontal" method="post" action="{{route('teams.members.invite', $team)}}">
-                            {!! csrf_field() !!}
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label class="control-label">E-Mail Address</label>
-
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                                <strong>{{ $errors->first('email') }}</strong>
-                                            </span>
+                <small class="text-muted">Members of team</small>
+                <h4>
+                    {{ $team->name }}
+                    <a href="{{ route('teams.index') }}" class="btn btn-sm btn-secondary float-right">
+                        <i class="fa fa-arrow-left"></i> Back
+                    </a>
+                </h4>
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th>Name</th>
+                        <th>Joined</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    @foreach($team->users as $user)
+                        <tr>
+                            <td>{{ $user->name }}</td>
+                            <td>
+                                <small class="text-muted">{{ $user->created_at->diffForHumans() }}</small>
+                            </td>
+                            <td>
+                                @if(auth()->user()->isOwnerOfTeam($team))
+                                    @if(auth()->user()->getKey() !== $user->getKey())
+                                        <form style="display: inline-block;"
+                                              action="{{route('teams.members.destroy', [$team, $user])}}"
+                                              method="POST">
+                                            @csrf @method('DELETE')
+                                            <button class="btn btn-danger btn-sm">
+                                                <i class="fa fa-trash-o"></i>
+                                                Delete
+                                            </button>
+                                        </form>
+                                    @endif
                                 @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
 
-                            </div>
+            <div class="col-md-4">
+                <small class="text-muted">Invite to team </small>
+                <small class="font-weight-bold">{{ $team->name }}</small>
+                <form class="form-horizontal" method="post" action="{{ route('teams.members.invite', $team) }}">
+                    @csrf
+                    <div class="form-group">
+                        <label class="control-label">E-Mail Address</label>
 
+                        <input type="email" class="form-control @error('email') is-invalid @enderror"
+                               name="email" value="{{ old('email') }}">
 
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block">
-                                    <i class="fa fa-btn fa-envelope-o"></i> Invite to Team
-                                </button>
-                            </div>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+
                     </div>
-                    </form>
-                </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">
+                            <i class="fa fa-btn fa-envelope-o"></i> Invite to Team
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <small class="text-muted">Pending invitations</small>
+                <table class="table table-striped">
+                    @if ($team->invites->count())
+                        <thead class="thead-dark">
+                        <tr>
+                            <th>E-Mail</th>
+                            <th>Sent</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                    @endif
+
+                    @forelse($team->invites as $invite)
+                        <tr>
+                            <td>{{ $invite->email }}</td>
+                            <td>
+                                <small class="text-muted">{{ $invite->created_at->diffForHumans() }}</small>
+                            </td>
+                            <td>
+                                <a href="{{ route('teams.members.resend_invite', $invite) }}"
+                                   class="btn btn-sm btn-default">
+                                    <i class="fa fa-envelope-o"></i> Resend invite
+                                </a>
+                            </td>
+                        </tr>
+                    @empty
+                        <div class="jumbotron jumbotron-fluid text-center">
+                            <div class="container">
+                                <h1 class="display-4">Oh!</h1>
+                                <p class="lead">You don't have pending invitations!</p>
+                            </div>
+                        </div>
+                    @endforelse
+                </table>
             </div>
         </div>
     </div>
-    </div>
 @endsection
+
