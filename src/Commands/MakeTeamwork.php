554f1e1cfb18d9de6c55c887932f73d3055c942c
oliuz <geekrosado@gmail.com>
2020-06-03 14:58:40 -0400

    Changed simple emails view to markdown mailable class and Added Mailable class

--- src/Commands/MakeTeamwork.php
+++ src/Commands/MakeTeamwork.php
@@ -36,12 +36,13 @@ class MakeTeamwork extends Command
     protected $description = 'Create Teamwork scaffolding files.';
 
     protected $views = [
-        'emails/invite.blade.php' => 'teamwork/emails/invite.blade.php',
+        'emails/invite-member.blade.php' => 'teamwork/emails/invite-member.blade.php',
+        'emails/resend-invite.blade.php' => 'teamwork/emails/resend-invite.blade.php',
         'members/list.blade.php' => 'teamwork/members/list.blade.php',
         'create.blade.php' => 'teamwork/create.blade.php',
         'edit.blade.php' => 'teamwork/edit.blade.php',
         'index.blade.php' => 'teamwork/index.blade.php',
-        'includes/messages.blade.php' => 'teamwork/includes/messages.blade.php'
+        'includes/messages.blade.php' => 'teamwork/includes/messages.blade.php',
     ];
 
     /**
@@ -112,6 +113,18 @@ class MakeTeamwork extends Command
                 )
             );
 
+            $this->info('Installed TeamInviteMember Mailable Class.');
+            file_put_contents(
+                app_path('Mail/Teamwork/TeamInviteMember.php'),
+                $this->compileMailableStub('TeamInviteMember')
+            );
+
+            $this->info('Installed TeamResendInvite Mailable Class.');
+            file_put_contents(
+                app_path('Mail/Teamwork/TeamResendInvite.php'),
+                $this->compileMailableStub('TeamResendInvite')
+            );
+
             $this->info('Installed TeamStoreRequest.');
             file_put_contents(
                 app_path('Http/Requests/Teamwork/TeamStoreRequest.php'),
@@ -162,6 +175,9 @@ class MakeTeamwork extends Command
         }
         if (! is_dir(app_path('Http/Requests/Teamwork'))) {
             mkdir(app_path('Http/Requests/Teamwork'), 0755, true);
+        }
+        if (! is_dir(app_path('Mail/Teamwork'))) {
+            mkdir(app_path('Mail/Teamwork'), 0755, true);
         }
         if (! is_dir(app_path('Models'))) {
             mkdir(app_path('Models'), 0755, true);
@@ -236,6 +252,21 @@ class MakeTeamwork extends Command
             '{{namespace}}',
             $this->getAppNamespace(),
             file_get_contents(__DIR__.'/../stubs/models/'.$stubName.'.stub')
+        );
+    }
+
+    /**
+     * Compile the mailable stub.
+     *
+     * @param $stubName
+     * @return string
+     */
+    protected function compileMailableStub($stubName)
+    {
+        return str_replace(
+            '{{namespace}}',
+            $this->getAppNamespace(),
+            file_get_contents(__DIR__.'/../stubs/mail/'.$stubName.'.stub')
         );
     }
 }
---
+++ src/stubs/views/emails/invite-member.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+# Hi!
+
+{{ $invite->team->owner->name }} has invited you to join their {{ $invite->team->name }} team!
+
+Click here to join:
+
+@component('mail::button', ['url' => url('teams/accept/'.$invite->accept_token)])
+    Accept
+@endcomponent
+
+Thanks,<br>
+{{ $invite->team->owner->name }} | {{ $invite->team->name }}
+@endcomponent
