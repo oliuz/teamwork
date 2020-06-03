a7c02852f6d49e0dadbd7d890ca12e1a668b3e39
oliuz <geekrosado@gmail.com>
2020-06-03 19:53:07 -0400

    Changed Edit view on teams

--- src/stubs/views/edit.blade.php
+++ src/stubs/views/edit.blade.php
@@ -4,29 +4,36 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Edit team {{$team->name}}</div>
+                <div class="card shadow-sm rounded">
+
                     <div class="card-body">
-                        <form class="form-horizontal" method="post" action="{{route('teams.update', $team)}}">
-                            <input type="hidden" name="_method" value="PUT"/>
-                            @csrf
+                        <small class="text-muted">Edit team</small>
+                        <a href="{{ route('teams.index') }}" class="btn btn-sm btn-secondary float-right">
+                            <i class="fa fa-arrow-left"></i> Back
+                        </a>
+                        <h4>
+                            {{ $team->name }}
 
-                            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                <label>Name</label>
+                        </h4>
+                        <form class="form-horizontal" method="POST" action="{{route('teams.update', $team)}}">
+                            @csrf @method('PUT')
 
-                                <input type="text" class="form-control" name="name"
-                                       value="{{ old('name', $team->name) }}">
+                            <div class="form-group">
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                                       value="{{ old('name', $team->name) }}"
+                                       placeholder="Enter name">
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
                                     </span>
-                                @endif
+                                @enderror
                             </div>
 
 
                             <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-block">
+                                <button type="submit" class="btn btn-primary btn-lg btn-block">
                                     <i class="fa fa-btn fa-save"></i>Save
                                 </button>
                             </div>
