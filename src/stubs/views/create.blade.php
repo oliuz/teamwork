133f2b594a270b8e71da7999a809ef89494db5cb
oliuz <geekrosado@gmail.com>
2020-06-03 20:06:15 -0400

    Changed Create modal view on teams

--- src/stubs/views/create.blade.php
+++ src/stubs/views/create.blade.php
@@ -1,31 +1,28 @@
-<div class="modal fade" id="createModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" id="createModalCenter" tabindex="-1" role="dialog"
+     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Create a new team</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <form class="form-horizontal" method="post" action="{{route('teams.store')}}">
+            <form class="form-horizontal" method="POST" action="{{route('teams.store', '#create')}}">
                 @csrf
                 <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h5 class="modal-title mb-3" id="exampleModalCenterTitle">Create a new team</h5>
 
+                    <div class="form-group">
 
-                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                        <label class="control-label">Name</label>
+                        <input id="team-name" type="text" class="form-control @error('name') is-invalid @enderror"
+                               name="name" value="{{ old('name') }}" placeholder="Enter name" required>
 
-
-                        <input type="text" class="form-control" name="name" value="{{ old('name') }}" required>
-
-                        @if ($errors->has('name'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                        @endif
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
 
                     </div>
-                    <button class="btn btn-primary btn-block">Save</button>
+                    <button class="btn btn-primary btn-block btn-lg">Save</button>
 
                 </div>
             </form>
